3f2db25c7c46908675758c08a21daacee5c9d629
web3allday <99647430+web3allday@users.noreply.github.com>
2022-11-19 12:18:35 +0000

    task 2 completed

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,30 +1,35 @@
 import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [data, setData] = useState(null)
-  const [isLoading, setLoading] = useState(false)
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true)
-  }, [])
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
 
-  if (isLoading) return <p className="text-gray-700 text-base">Fetching Posts...</p>
-  if (!data) return <p>No POSTS fetched</p>
+  if (isLoading)
+    return <p className="text-gray-700 text-base">Fetching Posts...</p>;
+  if (!data) return <p>No POSTS fetched</p>;
 
   return (
     <div>
       <h1 className="font-bold text-2xl m-2">POSTS</h1>
-      {data.map(post =>
+      {data.map((post) => (
         <div className="m-2 rounded overflow-hidden shadow-lg">
           <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">{post.title}</div>
             <p className="text-gray-700 text-base"> {post.body} </p>
           </div>
           <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-blue-700 mr-2 mb-2">#{post.id}</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-blue-700 mr-2 mb-2">
+              #{post.id}
+            </span>
           </div>
         </div>
-      )}
+      ))}
     </div>
-  )
+  );
 }
