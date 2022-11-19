496dc14d34278b24515be45abc28cb4cc0f9f8e9
web3allday <99647430+web3allday@users.noreply.github.com>
2022-11-19 11:25:30 +0000

    task 1 done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(0);
@@ -6,14 +6,19 @@ export default function Home() {
   useEffect(() => {
     const init = 1;
     setCount(init + count);
-  });
+  }, []);
 
-  const increaseScore = () => setCount(1);
+  const increaseScore = () => setCount(count);
 
   return (
     <div>
       <p>Counter: {count}</p>
-      <button className="border border-indigo-500 bg-indigo-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline" onClick={increaseScore}>Increase Count</button>
+      <button
+        className="border border-indigo-500 bg-indigo-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
+        onClick={() => setCount(count + 1)}
+      >
+        Increase Count
+      </button>
     </div>
   );
 }
